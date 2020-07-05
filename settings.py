7588b9122ea81881e8a2da7388a67c1b6aaca570
Jorge Cuaron <kerloom@yahoo.com>
2020-07-05 14:29:39 -0500

    Added CORS, script to customize

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wolfram_queries.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 db = SQLAlchemy(app)
 
 # TODO: https://stackoverflow.com/questions/24291933/sqlalchemy-object-already-attached-to-session
