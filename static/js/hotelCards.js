bbb0f3e90e337dc8638b9a754f5d955131416a52
Jorge Cuaron <kerloom@yahoo.com>
2021-01-18 14:10:19 -0600

    Add Real Restrictions + Fix Prod Bug

--- static/js/hotelCards.js
+++ static/js/hotelCards.js
@@ -28,6 +28,9 @@ async function getFlightCard() {
     const placeURL = encodeURI(`${BASE_URL}/api/v1/riesgo?lugar=${placeQuery}&lang=${lang}`);
     
     padre = document.getElementsByClassName('elementor-element elementor-element-4cf0e52 elementor-widget elementor-widget-text-editor')[0]
+    if(!padre) {
+        padre = document.getElementsByClassName('elementor-element elementor-element-506108c elementor-widget elementor-widget-text-editor')[0]
+    }
 
     res = await fetch(placeURL);
     htmlString = await res.text();
@@ -81,7 +84,10 @@ async function getHotelCard() {
 
     const placeURL = encodeURI(`${BASE_URL}/api/v1/riesgo?lugar=${placeQuery}&lang=${lang}`);
     
-    padre =padre = document.getElementsByClassName('elementor-element elementor-element-4cf0e52 elementor-widget elementor-widget-text-editor')[0]
+    padre = document.getElementsByClassName('elementor-element elementor-element-4cf0e52 elementor-widget elementor-widget-text-editor')[0]
+    if(!padre) {
+        padre = document.getElementsByClassName('elementor-element elementor-element-506108c elementor-widget elementor-widget-text-editor')[0]
+    }
 
     res = await fetch(placeURL);
     htmlString = await res.text();
